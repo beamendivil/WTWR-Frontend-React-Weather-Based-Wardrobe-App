e1e435a813f4ab758634661918ec6f3ec803ec06
beamendivil <bea@curatedtucson.com>
2026-04-02 11:47:54 -0700

    add production env file and normalize vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/se_project_react/" : "/",
-  server: {
-    port: 3000,
-  },
+  base: "/",
 });
